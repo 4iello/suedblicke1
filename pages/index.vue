dd39b77fac2f5819b93743adfd3f30c1aa38c067
4iello <alp.aiello@icloud.com>
2022-02-10 21:47:03 +0100

    version1

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="Worldmap">
+    <WorldMap :eyeAnimationFinished="eyeAnimationFinished" :topics="topics" />
+  </div>
+</template>
+
+<script>
+import WorldMap from "~/components/WorldMap.vue";
+export default {
+  components: { WorldMap },
+  async asyncData() {
+    const topics = await fetch("https://sblicke.uber.space/items/topic").then(
+      (res) => res.json()
+    );
+    return { topics };
+  },
+  data() {
+    return {
+      eyeAnimationFinished: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => (this.eyeAnimationFinished = true), 5000);
+  },
+};
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#Worldmap {
+}
+</style>
